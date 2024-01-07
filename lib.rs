448cb4e7fc95de7ddbfb4c88bf7682e3983deff1
jesulonimi21 <anjola4jeez@gmail.com>
2024-01-07 19:57:55 +0100

    Add: assert for owner updating code

--- lib.rs
+++ lib.rs
@@ -27,7 +27,7 @@ mod bugbite_presale {
     impl Token{
         #[ink(constructor)]
         pub fn new(price_per_token: Balance, presale_token: AccountId) -> Self{
-            assert!(price_per_token > 0);
+            assert!(price_per_token > 0, "price per token must be greater than zero");
             let caller = Self::env().caller();
             Self { price_per_token,owner: caller,  presale_asset: presale_token }
         }
@@ -44,11 +44,11 @@ mod bugbite_presale {
 
         #[ink(message, payable)]
         pub fn buy_token(&mut self, amount_to_purchase: Balance) -> Balance{
-            assert!(amount_to_purchase > 0);
+            assert!(amount_to_purchase > 0, "amount to purchase must be greater than zero");
             let from = self.env().caller();
             let price: Balance = (amount_to_purchase * self.price_per_token)/1000_000_000_000;
             let transferred_value = self.env().transferred_value();
-            assert!(transferred_value >= price);
+            assert!(transferred_value >= price, "value lesser than price");
             Self::env().transfer(self.owner, price).expect("Could not send native tokens to the owner");
             let mut token: contract_ref!(PSP22) = self.presale_asset.into();
             let to_balance_before = token.balance_of(from);
@@ -56,7 +56,7 @@ mod bugbite_presale {
             let _ = token.transfer(from, price,  Vec::<u8>::new());
             let to_balance = token.balance_of(from);
             let new_balance = to_balance - to_balance_before;
-            assert_eq!(new_balance, price);
+            assert_eq!(new_balance, price, "the senders balance does not change");
             if transferred_value > price {
                 Self::env().transfer(from, transferred_value - price).expect("could not transfer excess price sent by user");
             };
@@ -68,7 +68,7 @@ mod bugbite_presale {
 
         #[ink(message)]
         pub fn claim_tokens(&mut self, amount_tokens: Balance) -> Balance{
-            assert_eq!(self.env().caller(), self.owner);
+            assert_eq!(self.env().caller(), self.owner,  "only admin can call this function");
             let mut token: contract_ref!(PSP22) = self.presale_asset.into();
             let _ = token.transfer(self.owner, amount_tokens,  Vec::<u8>::new());
             amount_tokens
@@ -84,6 +84,7 @@ mod bugbite_presale {
 
         #[ink(message)]
         pub fn set_code(&mut self, code_hash: [u8; 32]) {
+            assert_eq!(self.env().caller(), self.owner, "only admin can call this function");
             ink::env::set_code_hash(&code_hash).unwrap_or_else(|err| {
                 panic!(
                     "Failed to `set_code_hash` to {:?} due to {:?}",
